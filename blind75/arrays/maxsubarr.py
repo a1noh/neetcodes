c86cd6333292477fe0460e74fc9f275c1c04d67c
a1noh <noh1austin@gmail.com>
2022-01-06 13:44:55 -0500

    comments on max subarray

--- blind75/arrays/maxsubarr.py
+++ blind75/arrays/maxsubarr.py
@@ -1,3 +1,7 @@
+# Link: https://www.youtube.com/watch?v=5WZl3MMT0Eg&list=PLot-Xpze53ldVwtstag2TL4HQhAnC8ATf&index=5
+# Title: Maximum subarray
+# Leetcode: https://leetcode.com/problems/maximum-subarray/
+
 def maxSubArray(nums):
     max_sum = nums[0]
     cur_sum = 0
